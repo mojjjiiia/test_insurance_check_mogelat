d2fade7e1e0e7d43e63c4d5ca15475c3345b2661
mojjjiiia <52158176+mojjjiiia@users.noreply.github.com>
2019-10-21 10:21:04 +0300

    Created Polis model

--- medask/polischeck/models.py
+++ medask/polischeck/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Polis(models.Model):
+    
+    POLIS_TYPES = 
+        [
+            ('OMS', 'OMS'),
+            ('DMS', 'DMS'),
+        ]
+    
+    company_name = models.CharField(max_length=255)
+    num_regex = models.CharField(max_length=50)
+    polis_type = models.CharField(max_length=3, choices=POLIS_TYPES, default='OMS')
+    
