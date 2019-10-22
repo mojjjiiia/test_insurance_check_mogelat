ef2fd4f037495b1d161a0818be6ed78782f6f6bd
mojjjiiia <52158176+mojjjiiia@users.noreply.github.com>
2019-10-22 13:56:25 +0300

    Update views.py

--- medask/polischeck/views.py
+++ medask/polischeck/views.py
@@ -3,7 +3,7 @@ from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Polis, num_check, service_check, request_to_sk, request_to_service_db
-from .serializers import PolisSerializer
+import json
 
 class SearchView(APIView):
     def get(self, request):
@@ -47,7 +47,7 @@ class PolisCheckView(APIView):
             polis_ltd_notfoundservice,
         ) = request_to_service_db(services)
         
-        return Response(
+        return Response(json.dumps(
             {
                  'polis_ltd_sk': polis_ltd_sk, 
                  'polis_ltd_type': polis_ltd_type, 
@@ -57,5 +57,5 @@ class PolisCheckView(APIView):
                  'polis_ltd_inservice': polis_ltd_inservice,
                  'polis_ltd_notservice': polis_ltd_notservice,
                  'polis_ltd_notfoundservice': polis_ltd_notfoundservice,
-            }
+            }, indent=4)
         )
