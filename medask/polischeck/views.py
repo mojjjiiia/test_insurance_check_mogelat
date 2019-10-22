154553092c299fec84870285979c3690bcdffe99
mojjjiiia <52158176+mojjjiiia@users.noreply.github.com>
2019-10-22 08:56:57 +0300

    Update views.py

--- medask/polischeck/views.py
+++ medask/polischeck/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Polis
+from .models import Polis, num_check, service_check
 from .serializers import PolisSerializer
+
+
+class SearchView(APIView):
+    def get(self, request):
+        number = request.GET.get('number', None)
+        service = request.GET.get('service', None)
+      
+        if number:
+            resp = {}
+            try:
+                resp['company'], resp['polis_type'] = num_check(number)
+                return Response(resp)
+            except ObjectDoesNotExist:
+                return Response(resp)
+        
+        if service:
+            in_database = service_check(service)
+            return Response({'service_in_base': in_database})        
 
 
 class PolisView(APIView):
