306b04c79345daf68b8719f48a6e59226c6f16f1
mojjjiiia <52158176+mojjjiiia@users.noreply.github.com>
2019-10-22 09:06:05 +0300

    Update urls.py

--- medask/polischeck/urls.py
+++ medask/polischeck/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
-from .views import PolisView
+from django.urls import path, re_path
+from .views import PolisView, SearchView
 
 
 app_name = 'polischeck'
 
 urlpatterns = [
+    re_path(r'^polischeck/search\?\w.*', SearchView.as_view()),
     path('polischeck/', PolisView.as_view()),
     ]
