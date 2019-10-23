551bacb827fb67cb9a307b7553bf8e7cadbddaee
mojjjiiia <52158176+mojjjiiia@users.noreply.github.com>
2019-10-23 09:00:14 +0300

    Update serializers.py

--- medask/polischeck/serializers.py
+++ medask/polischeck/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import Polis
 
 
-class PolisSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Polis
-        fields = ('company_name', 'polis_type', 'num_regex')
+class PolisSerializer(serializers.Serializer):
+    choices = [('OMS', 'OMS'), ('DMS', 'DMS')]
+    
+    company = serializers.CharField(max_length = 255)
+    polis_number = serializers.CharField(max_length = 50) 
+    polis_type = serializers.ChoiceField(choices)
+    services = serializers.CharField(max_length = 255)     
