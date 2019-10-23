8246ec89078c7ee2c23f610a626ff4b539f1f526
mojjjiiia <52158176+mojjjiiia@users.noreply.github.com>
2019-10-23 11:00:54 +0300

    Update models.py

--- medask/polischeck/models.py
+++ medask/polischeck/models.py
@@ -18,6 +18,7 @@ class Polis(models.Model):
     num_regex = models.CharField(max_length=50)
     polis_type = models.CharField(max_length=3, choices=POLIS_TYPES, default='OMS')
     BILLING = models.IntegerField(default=0)
+    not_found_search = models.IntegerField(default=0)
     
     def __str__(self):
         return self.company_name + ' ' + self.polis_type
