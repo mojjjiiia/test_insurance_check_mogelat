1b2c4967a2c5eda6d2e77df7c08084024e707a45
mojjjiiia <52158176+mojjjiiia@users.noreply.github.com>
2019-10-23 21:13:03 +0300

    Updated comments

--- medask/polischeck/models.py
+++ medask/polischeck/models.py
@@ -1,21 +1,19 @@
 from django.db import models
 
 
-class Polis(models.Model): # Модель страхового полиса, хранящую формат номера полиса вместо самого номера
+class Polis(models.Model): # Модель страхового полиса, хранящая формат номера полиса вместо самого номера
 
     POLIS_TYPES = [
             ('OMS', 'OMS'),
             ('DMS', 'DMS'),
         ] # Доступные типы страховых полисов
     
-    company = models.CharField(max_length=255) # Имя компании
+    company = models.CharField(max_length=255) # Имя страховой компании
     num_regex = models.CharField(max_length=50) # Регулярное выражение,
-    # для определения принадлежности полиса к компании по его номеру.
+                                                # для определения принадлежности полиса к компании по его номеру.
     polis_type = models.CharField(max_length=3, choices=POLIS_TYPES, default='OMS')
-    BILLING = models.IntegerField(default=0) # Счетчик успешных запросов по данному формату номера полисв
-    not_found_search = models.IntegerField(default=0) # Счетчик не успешных запросов (номер не найден в БД страх. комп.)
+    BILLING = models.IntegerField(default=0) # Счетчик успешных запросов по данному формату (regexp) номера полисов
+    not_found_search = models.IntegerField(default=0) # Счетчик НЕ успешных запросов (номер не найден в БД страх. комп.)
     
     def __str__(self):
         return self.company + ' ' + self.polis_type
-
-
